a7ba96775bc859dc8f0b524147187757048de763
harsh021102 <harsh021102@gmail.com>
2022-04-26 18:38:32 +0530

    Updates

---
+++ precache-manifest.0fbc797fd6fe443765cddfba199fbd31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9119fd53a1bcf6284dcc",
+    "url": "/color-flipper/static/css/main.f8fe4e27.chunk.css"
+  },
+  {
+    "revision": "9119fd53a1bcf6284dcc",
+    "url": "/color-flipper/static/js/main.9119fd53.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/color-flipper/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "9d5068ca31900dd29738",
+    "url": "/color-flipper/static/js/2.9d5068ca.chunk.js"
+  },
+  {
+    "revision": "cde989440fa756af5b11",
+    "url": "/color-flipper/static/js/runtime~main.cde98944.js"
+  },
+  {
+    "revision": "99fb1e9252b29613ad961d342b12ccee",
+    "url": "/color-flipper/index.html"
+  }
+];
